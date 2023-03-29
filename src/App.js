2d6e88a76eb94f22396c7beb2c88d37b59644ba6
arielmalada <arielmalada@outlook.com>
2023-03-30 00:12:27 +0300

    feat: task6 completion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Task5 from './views/Task5';
+import Task6 from './views/Task6';
 
 function App() {
   return (
-    <Task5 />
+    <Task6 />
   );
 }
 
